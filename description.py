221331b03bf3e1a2b3380c015aa0b876b9b00895
drakefan11 <160178311+drakefan11@users.noreply.github.com>
2025-12-04 11:29:42 -0500

    Update description.py

--- description.py
+++ description.py
@@ -1,6 +1,8 @@
 from functions import *
 from making_df import *
 
+df = df.drop(['release_date','box_office','run_time','languages','countries','genres'], axis=1)
+
 def description_search(query,embeds_list,mov_sums,score_list):
     # make a list of the words for the query
     querywords = []
@@ -45,11 +47,10 @@ def description_search(query,embeds_list,mov_sums,score_list):
         # make a whole column in the new df for scores to the user's prompt
         plot_summaries['scores'] = score_list
 
-        # sort them from highest score to lowest
-        sorted_summs = plot_summaries.sort_values(by='scores', ascending=False)
-
-        # print them out
-        print(sorted_summs['name'].head(10))
+        sorted_summs = pd.merge(df,plot_summaries,on="wik_mID",how="inner")
+        sorted_summs = sorted_summs.sort_values(by='scores', ascending=False)
+        only_name = sorted_summs[['name','summary','scores']]
+        print(only_name.head(10))
 
         # clear everything
         querywords.clear()
