ec5aad1b2a2d3ad77d4f6b65e12af0a40c63b2b2
drakefan11 <160178311+drakefan11@users.noreply.github.com>
2025-12-07 22:25:07 -0500

    Update description.py

--- description.py
+++ description.py
@@ -1,12 +1,19 @@
+# import the functions from the .py files and df
 from functions import *
 from making_df import *
 
+# reference the master df
 df = df.drop(['release_date','box_office','run_time','languages','countries','genres'], axis=1)
 
+# a function that gets the description of the movie
 def description_search(query,embeds_list,mov_sums,score_list):
+    # initialize the query count
     q_count = 0
+
     # make a list of the words for the query
     querywords = []
+
+    # clean the query
     queryclean = cleanquery(query)
     
     # for each word grab its vector
@@ -14,11 +21,16 @@ def description_search(query,embeds_list,mov_sums,score_list):
 
         # add it to a list
         querywords.append(embeds_list[[i]])
+        
+        # the count of words in the query
         q_count += 1
 
         # sum all of the vectors in that list
         v1 = sum(querywords)
+
+        # get an average value
         v1 = v1 / q_count
+
         # for each cleaned summaries
         for i in range(len(mov_sums)):
 
@@ -48,8 +60,10 @@ def description_search(query,embeds_list,mov_sums,score_list):
         # make a whole column in the new df for scores to the user's prompt
         plot_summaries['scores'] = score_list
 
+        # making a new df to sort the scores and titles most similar to the query the user typed in
         sorted_summs = pd.merge(df,plot_summaries,on="wik_mID",how="inner")
         sorted_summs = sorted_summs.sort_values(by='scores', ascending=False)
+
         only_name = sorted_summs[['name','summary','scores']]
         print(only_name.head(10))
 
