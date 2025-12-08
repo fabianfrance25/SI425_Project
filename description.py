ac9b0b6aa1c32bbaf8ce07465adca7fe24f2f9f0
fabianfrance25 <m262106@usna.edu>
2025-12-07 22:04:32 -0500

    Description.py update

--- description.py
+++ description.py
@@ -3,55 +3,51 @@ from making_df import *
 
 df = df.drop(['release_date','box_office','run_time','languages','countries','genres'], axis=1)
 
-def description_search(query,embeds_list,mov_sums,score_list):
-    # make a list of the words for the query
-    querywords = []
+def description_search(query, embeds_list, mov_sums, score_list):
+    global q_count
+
+    # clean + tokenize
     queryclean = cleanquery(query)
+
+    # build query embedding
+    query_vec = numpy.zeros(300)
+    count = 0
+
+    for word in queryclean:
+        if word in embeds_list:
+            query_vec += embeds_list[word]
+            count += 1
     
-    # for each word grab its vector
-    for i in range(len(queryclean)):
+    if count == 0:
+        print("No valid words found in description.")
+        return
+    
+    query_vec /= count
 
-        # add it to a list
-        querywords.append(embeds_list[[i]])
-        q_count += 1
+    # compute similarity with each movie
+    score_list.clear()
 
-        # sum all of the vectors in that list
-        v1 = sum(querywords)
-        v1 = v1 / q_count
-        # for each cleaned summaries
-        for i in range(len(mov_sums)):
+    for summary_words in mov_sums:
+        v2 = numpy.zeros(300)
+        counter = 0
 
-            # get the cleaned summary
-            summed = mov_sums[i]
+        for w in summary_words:
+            if w in embeds_list:
+                v2 += embeds_list[w]
+                counter += 1
 
-            # make the vector of zeros
-            v2 = numpy.zeros(300)
-            
-            # making a counter to get the average value
-            counter = 0
-
-            # for each word in the summary
-            for words in summed:
-
-                # if it exists, add it to the vector
-                if words in embeds_list:
-                    v2 += embeds_list[words]
-                    counter +=1
-            
-            # get an average value for the vector
+        if counter > 0:
             v2 /= counter
+            sim = cosine(query_vec.flatten(), v2.flatten())
+        else:
+            sim = 0
+        
+        score_list.append(sim)
 
-            # add all of the similarity scores to a list
-            score_list.append(cosine(v1.flatten(),v2.flatten()))
+    # save to dataframe
+    plot_summaries['scores'] = score_list
 
-        # make a whole column in the new df for scores to the user's prompt
-        plot_summaries['scores'] = score_list
-
-        sorted_summs = pd.merge(df,plot_summaries,on="wik_mID",how="inner")
-        sorted_summs = sorted_summs.sort_values(by='scores', ascending=False)
-        only_name = sorted_summs[['name','summary','scores']]
-        print(only_name.head(10))
-
-        # clear everything
-        querywords.clear()
-        score_list.clear()
+    sorted_summs = pd.merge(df, plot_summaries, on="wik_mID", how="inner")
+    sorted_summs = sorted_summs.sort_values(by='scores', ascending=False)
+    only_name = sorted_summs[['name','summary','scores']]
+    print(only_name.head(10))
---
+++ other_factors.py
@@ -0,0 +1,4 @@
+def box_office_revenue(revenue):
+    factor = revenue / (100000000) # divided by the average
+    return factor
+
