f662cccd5b2be394c4bb4a46c244b662a9359773
drakefan11 <160178311+drakefan11@users.noreply.github.com>
2025-12-08 21:47:51 -0500

    Update movsearch.py
    added comments and changed the weights to be more even

--- movsearch.py
+++ movsearch.py
@@ -1,10 +1,9 @@
 import nltk
 import pandas as pd
-import gensim.downloader as api
 from description import description_search
 from mood import mood_search
-from year_pref import compute_year_pref
 from making_df import *
+from functions import *
 from gensim.models.word2vec import Word2Vec
 
 nltk.download('vader_lexicon')
@@ -22,44 +21,50 @@ movsearch_model = Word2Vec(sentences=Msumcleaned,
 # grab the word vectors for each word
 embeds = movsearch_model.wv
 
+# lists of all the weights
+genrelist = ['Romance', 'Comedy', 'Musical', 'Family', 'Animation', 'Drama', 'Biographical film', 'History', 'Documentary', 'Adventure', 'Sports', 'Short Film', 'Western', 'Anime', 'Japanese Movies', 'Action', 'Science Fiction', 'Crime Fiction', 'Crime Drama', 'Mystery', 'Supernatural', 'Space western', 'Thriller', 'Horror', 'War']
+moodlist = ['happy', 'joy', 'funny', 'sad', 'dark', 'intense', 'exciting', 'romantic', 'scary', 'action']
+
+# a grabs all of the plot summaries and cleans them as input for the recommender
 summaries = plot_summaries['summary'].tolist()
 cleansumms = [cleanfilm(s) for s in summaries]
 
-# Ask User Queries
+# ask the user for input given a desired genre, mood, and a current or older movie
+print(f'Here are the different genres you can pick from: {genrelist}\n')
 desc_query = input("Describe the type of movie you want (genre): ")
+
+print(f'Here are the different moods you can pick from: {moodlist}\n')
 tone_query = input("Describe the mood (happy, sad, intense, etc.): ")
+
 year_query = input("Do you prefer NEW movies or OLD movies? ").lower().strip()
 
-
-# make a list of plot scores
-p_scores = []
-
-# Compute Feature Scores
+# computing the feature scores for each input
 desc_df = description_search(desc_query, embeds, cleansumms)  
 mood_df = mood_search(tone_query, cleansumms)               
 
-# Merge Scores
+# merging them
 merged = desc_df.merge(mood_df[['wik_mID','mood_score']], on='wik_mID')
 
-# Merge Original Metadata
+# Mmerging them to the original metadata
 merged = merged.merge(df[['wik_mID','name','release_date']], on='wik_mID', how='left')
 merged['year'] = pd.to_datetime(merged['release_date'], errors='coerce').dt.year
 
-# Year preference
+# computing the year preferance
 merged = compute_year_pref(merged, year_query)  # adds 'year_sim'
 
-# Weighted Final Ranking
-W_DESC = 0.6
+# adjustable weights for our own funciton
+W_DESC = 0.5
 W_SENT = 0.3  
-W_YEAR = 0.1
+W_YEAR = 0.2
 
+# producing the final score given the weights
 merged["final_score"] = (
     W_DESC * merged["desc_sim"] +
     W_SENT * merged["mood_score"] +
     W_YEAR * merged["year_sim"]
 )
 
-# Sort and Display 
+# sort and display the recommendations
 merged = merged.sort_values(by="final_score", ascending=False)
 print("\nTop 10 Recommended Movies:\n")
 print(merged[['name','summary','final_score']].head(10))
