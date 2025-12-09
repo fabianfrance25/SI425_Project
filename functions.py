35d948e9e06682a49e2d02b9c481967cd36fe9b2
drakefan11 <160178311+drakefan11@users.noreply.github.com>
2025-12-08 21:46:33 -0500

    Update functions.py
    added more function for year preference

--- functions.py
+++ functions.py
@@ -59,3 +59,10 @@ def cleanfilm(summary):
 
     # return the cleaned summary
     return cleansum
+
+def compute_year_pref(df, preference):
+    target_year = 2025 if preference == "new" else 1950
+
+    df["year_sim"] = 1 - abs(df["year"] - target_year) / 40
+    df["year_sim"] = df["year_sim"].clip(0, 1)
+    return df
